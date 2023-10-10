d5203067cac872392d36f17450ccc1717e3c9298
Eduardo <eduaraujp760@gmail.com>
2023-10-10 13:08:35 -0400

    atividade resolvida

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./Components/Button";
+
+function App() {
+  return (
+    <>
+      <Button className="btn" type="button" disabled={true}>
+        teste
+      </Button>
+    </>
+  );
+}
+
+export default App;
